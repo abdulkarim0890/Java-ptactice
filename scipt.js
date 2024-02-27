1b73c89757ad9e0c4914205eb9cdad1dd1b120a9
abdulkarim <abdulkarim140890@gmail.com>
2024-02-27 19:16:40 +0500

    Initial commit

---
+++ scipt.js
@@ -0,0 +1,153 @@
+//alert("ABC")
+
+//var a ="ABC"
+//console.log(b)
+
+//var b  =20
+//console.log(b)
+//var b  =30
+//console.log(b)
+
+//var a = 10
+//var b = 50 - a
+//console.log(b)
+
+//var a = 90-30
+//console.log(a)
+//var a = 10 * (5 + 30)
+//console.log(a)
+
+//var a = "30"
+//var b = 20
+//var c = a - b
+//console.log(c) 
+//can perform
+
+//var a = "30"
+//var b = 20
+//var c = a + b
+//console.log(c)
+//cannot perform 
+
+//var a = "Abdul"
+//var b = "Karim"
+//var c = a+" "+b
+//console.log(c) 
+
+//pre increment
+//var a = 10
+//var b = ++a
+//console.log(b)
+
+//post increment
+//var a = 10
+//var b = a++
+//console.log(b)
+//console.log(a)
+
+//var a = prompt("enter first name")
+//var b = prompt("enter last name")
+//var c = prompt("enter pasword")
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+//var a = +prompt("english")
+//var b = +prompt("urdu")
+//var c = +prompt("islamiat")
+//var d = +prompt("maths")
+//var e = +prompt("sindhi")
+//console.log(a,b,c,d,e)
+//console.log(a + b + c + d + e)
+
+//var asd = '' //leagal
+//var 123 = '' //illeagal
+//var ab12 = ''//leagal
+//var 12ab = '' //illeagal
+//var var = '' //illeagal
+//var ab@s = ''//illeagal
+//var ab_ = ''//leagal
+
+//var a = 10*5
+//var b = 45 
+//if(a == b){
+  //  console.log("a is greater b you can do action")
+//}else{console.log("a is not equal to b")}
+
+//var marks = 70
+//if(marks > 70){
+  //  console.log("Mithai")
+//}else{
+  //  console.log("Dhulai")
+//}
+
+// var marks = 70
+//if(marks > 70){
+  //  console.log("Mithai")
+//}else if(marks > 50){
+  //  console.log("amma ne bacha lia")
+//}else{
+  //  console.log("dono se dhulai")
+//}
+
+var a = +prompt("enter english marks")
+var b = +prompt("enter urdu marks")
+var c = +prompt("enter islamiat marks")
+var d = +prompt("enter maths marks")
+var e = +prompt("enter sindhi marks")
+console.log(a,b,c,d,e)
+console.log((a + b + c + d + e ) /500 *100)
+
+var total =((a + b + c + d + e ) /500 *100)
+if(total >= 80){
+console.log("A1 Grade")
+}else if(total >= 70){
+  console.log("A Grade")
+}else if(total >= 60){
+  console.log("B Grade")
+}else if(total >= 50){
+  console.log("C Grade")
+}
+
+//var a="10"
+//if(a===10){
+//  console.log('A is Equal')
+//}
+
+
+//var StudentCard = true
+//var dept ="sci"
+//if(studentcard == true){
+  //  if(dept == "sci"){
+    //    console.log(Allow)
+    //}
+//}else{
+  //  console.log("not allow")
+//}
+//else{
+  //  console.log("not allow")
+//}
+//if(studentcard == true && dept == 'sci'){
+  //  console.log("Allow")
+//}else{
+    //console.log(Not Allow)
+//}
+//if(studentcard == true || dept == 'sci'){
+  //  console.log("Allow")
+//}else{
+    //console.log(Not Allow)
+//}
+
+//var students =["zaid","haris","fazal"]
+//console.log(students[1])
+//students[6] = "ahmed"
+//console.log(students)
+
+//var students =["zaid","haris","fazal"]
+//console.log(students[1])
+//students[6] = "ahmed"
+//students.push("abu bakar")
+//students.pop()
+//students.unshift("abdullah")
+//students.shift()
+//console.log(students)
